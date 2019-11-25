fbbd3abac5092ed975aa43094231fbba48fd0849
Praba <yyyypraba.wg@gmail.com>
2019-11-25 12:43:18 +0530

    angular cli

--- client/src/app/_services/authentication.service.ts
+++ client/src/app/_services/authentication.service.ts
@@ -2,6 +2,7 @@
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '@environments/environment';
 
 import { User } from '@/_models';
 
@@ -20,13 +21,13 @@ export class AuthenticationService {
     }
 
     login(username, password) {
-        return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
-            .pipe(map(user => {
-                // store user details and jwt token in local storage to keep user logged in between page refreshes
-                localStorage.setItem('currentUser', JSON.stringify(user));
-                this.currentUserSubject.next(user);
-                return user;
-            }));
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+        .pipe(map(user => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+            this.currentUserSubject.next(user);
+            return user;
+        }));
     }
 
     logout() {
